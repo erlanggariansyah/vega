ea1b05f65f12160e7f87437a5737d18205b9e2cb
Erlangga Riansyah <erlanggariansyah0@gmail.com>
2024-03-11 23:50:35 +0700

    Register

--- app/src/main/java/com/erlanggariansyah/vega/activities/LoginActivity.kt
+++ app/src/main/java/com/erlanggariansyah/vega/activities/LoginActivity.kt
@@ -1,10 +1,15 @@
 package com.erlanggariansyah.vega.activities
 
+import android.app.Dialog
 import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.erlanggariansyah.vega.R
@@ -12,6 +17,7 @@ import com.erlanggariansyah.vega.R
 class LoginActivity : AppCompatActivity() {
     private lateinit var loginBack: ImageView
     private lateinit var loginSubmit: Button
+    private lateinit var registerButton: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +25,7 @@ class LoginActivity : AppCompatActivity() {
 
         loginBack = findViewById(R.id.login_back)
         loginSubmit = findViewById(R.id.login_submit_button)
+        registerButton = findViewById(R.id.register_button)
     }
 
     override fun onResume() {
@@ -42,6 +49,39 @@ class LoginActivity : AppCompatActivity() {
             alertDialogBuilder
                 .create()
                 .show()
+        }
+
+        val registerDialog = Dialog(this)
+        registerButton.setOnClickListener {
+            registerDialog.setContentView(R.layout.activity_login_register)
+            registerDialog.window?.setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+            registerDialog.window?.attributes?.windowAnimations = R.style.animation
+
+            val registerQR = registerDialog.findViewById<View>(R.id.register_qr)
+            registerQR.setOnClickListener {
+                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivity(intent)
+            }
+
+            val registerIndividual = registerDialog.findViewById<View>(R.id.register_individual)
+            registerIndividual.setOnClickListener {
+                val intent = Intent(this@LoginActivity, RegisterIndividualActivity::class.java)
+                startActivity(intent)
+            }
+
+            val registerCorporate = registerDialog.findViewById<View>(R.id.register_corporate)
+            registerCorporate.setOnClickListener {
+                val intent = Intent(this@LoginActivity, RegisterCorporateActivity::class.java)
+                startActivity(intent)
+            }
+
+            val registerDPLK = registerDialog.findViewById<View>(R.id.register_dplk)
+            registerDPLK.setOnClickListener {
+                val intent = Intent(this@LoginActivity, RegisterDPLKActivity::class.java)
+                startActivity(intent)
+            }
+
+            registerDialog.show()
         }
     }
 }
---
+++ app/src/main/java/com/erlanggariansyah/vega/activities/RegisterIndividualActivity.kt
@@ -0,0 +1,37 @@
+package com.erlanggariansyah.vega.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import com.erlanggariansyah.vega.R
+
+class RegisterIndividualActivity : AppCompatActivity() {
+    private lateinit var registerBack: ImageView
+    private lateinit var login: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_individual)
+
+        registerBack = findViewById(R.id.register_back)
+        login = findViewById(R.id.login_button)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        registerBack.setOnClickListener {
+            val intent = Intent(this@RegisterIndividualActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        login.setOnClickListener {
+            val intent = Intent(this@RegisterIndividualActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
